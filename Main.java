bc73be3db38a7990f75fb2ee03d4eb1eeb8118b1
Liali4 <liaalbano7@gmail.com>
2026-08-12 22:26:30 -0300

    lulu

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
 public class Main {
 public static void main (String[] args) {
-    System.out.println("Hello World!");
+    String aluno_1 = "Luis";
+    String aluno_2 = "Miguel";
+    System.out.println("o aluno 1: " + aluno_1);
+    System.out.println("o aluno 2: " + aluno_2);
+    System.out.println(aluno_1 + " e o " + aluno_2);
 }
 
 }
