ab0485fc0b1789c21f8d6b17f88b1015cafcce9f
Liali4 <liaalbano7@gmail.com>
2026-08-19 22:20:35 -0300

    folders

---
+++ 3_operadores/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+        public static void main (String[] args){
+// int valor_1 = 10;
+// int valor_2 = 20;
+// int soma = 0;
+// soma = valor_1 + valor_2;
+// System.out.println("O valor 1 e: " + valor_1);
+// System.out.println("O valor 2 e: " + valor_2);
+// System.out.println("A soma e: " + soma);
+
+// double valor = 6;
+// System.out.println(valor % 2);
+
+int contador = 0;
+System.out.println(contador++);
+System.out.println(contador--);
+        }
+    
+}
---
+++ 4_strings/Main.java
@@ -0,0 +1,13 @@
+
+
+public class Main {
+    public static void main(String[] args){
+        String saudacao = "Hello world";
+        System.out.println(saudacao.length());
+        System.out.println(saudacao.toUpperCase());
+        System.out.println(saudacao.toLowerCase());
+        System.out.println(saudacao.indexOf("w"));
+        System.out.println(saudacao.charAt(6));
+    }
+    
+}
