a00c0f4fdad2e7ffcfb23eb637c658f0bca42607
Liali4 <liaalbano7@gmail.com>
2026-09-16 20:03:09 -0300

    socorro

---
+++ Método/Exercicio_welcome/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    static void welcomeMessage(){
+        System.out.println("Bem-vindo ao Java!");
+
+    }
+    public static void main(String[]args){
+        welcomeMessage();
+        welcomeMessage();
+    }
+
+}
---
+++ Método/Main.java
@@ -0,0 +1,25 @@
+public class Main{
+    static void meuMetodo(){//funçao
+        System.out.println("socorro");
+
+    }
+    static void metodoMeu(){
+        System.out.println("dessocorro");
+
+    }
+    static void metodo(){
+        System.out.println("cosorro");
+
+    }
+    public static void main(String[]args){
+
+        meuMetodo();
+        meuMetodo();
+        metodoMeu();
+        metodo();
+
+
+
+
+    }
+}
