7b41cfded2d959ccc4075e83ee4791e7e8c28c61
Liali4 <liaalbano7@gmail.com>
2026-09-16 20:13:49 -0300

    parametro

---
+++ Parametros/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    static void parametro(String nome, int idade){
+        System.out.println(nome + " possui " + idade + " anos");
+    }
+    public static void main(String[]args){
+
+    parametro("Julia",  15);
+    }
+    
+}
